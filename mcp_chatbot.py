161472086b01bd3f934f032e1dcdca83d713eb4b
Cristian Carpenter <ccarpenterg@gmail.com>
2026-03-27 00:46:35 -0300

    added mcp chatbot client and imports

---
+++ mcp_chatbot.py
@@ -0,0 +1,9 @@
+from dotenv import load_dotenv
+from anthropic import Anthropic
+from mcp import ClientSession, StdioServerParameters, types
+from mcp.client.stdio import stdio_client
+from typing import List, Dict, TypedDict
+from contextlib import AsyncExitStack
+import json
+import asyncio
+
